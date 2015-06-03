728619319497c5b3bd8222c6618b83f5b333cbe3
zhichun <zhchcai@mytijian.com>
2015-06-03 18:05:55 +0800

    first commit

---
+++ src/main/java/com/mytijian/my_test_git_scm/App.java
@@ -0,0 +1,13 @@
+package com.mytijian.my_test_git_scm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
